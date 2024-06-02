17ef39e65bee89659e19245bd0e11b452c607843
lidiamoryc <lidiamoryc@onet.pl>
2024-06-03 00:43:52 +0200

    wersja z działającą todo listą

---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,392 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Todo.Models;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Todo.Models.ViewModels;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var todoListViewModel = GetAllTodos();
+            return View(todoListViewModel);
+        }
+
+        [HttpGet]
+        public JsonResult PopulateForm(int id)
+        {
+            var todo = GetById(id);
+            return Json(todo);
+        }
+
+
+        internal TodoViewModel GetAllTodos()
+        {
+            List<TodoItem> todoList = new();
+
+            using (SqliteConnection con =
+                   new SqliteConnection("Data Source=db.sqlite"))
+            {
+                using (var tableCmd = con.CreateCommand())
+                {
+                    con.Open();
+                    tableCmd.CommandText = "SELECT * FROM todo";
+
+                    using (var reader = tableCmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                todoList.Add(
+                                    new TodoItem
+                                    {
+                                        Id = reader.GetInt32(0),
+                                        Name = reader.GetString(1)
+                                    });
+                            }
+                        }
+                        else
+                        {
+                            return new TodoViewModel
+                            {
+                                TodoList = todoList
+                            };
+                        }
+                    };
+                }
+            }
+
+            return new TodoViewModel
+            {
+                TodoList = todoList
+            };
+        }
+
+        internal TodoItem GetById(int id)
+        {
+            TodoItem todo = new();
+
+            using (var connection =
+                   new SqliteConnection("Data Source=db.sqlite"))
+            {
+                using (var tableCmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    tableCmd.CommandText = $"SELECT * FROM todo Where Id = '{id}'";
+
+                    using (var reader = tableCmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            reader.Read();
+                            todo.Id = reader.GetInt32(0);
+                            todo.Name = reader.GetString(1);
+                        }
+                        else
+                        {
+                            return todo;
+                        }
+                    };
+                }
+            }
+
+            return todo;
+        }
+
+        public RedirectResult Insert(TodoItem todo)
+        {
+            using (SqliteConnection con =
+                   new SqliteConnection("Data Source=db.sqlite"))
+            {
+                using (var tableCmd = con.CreateCommand())
+                {
+                    con.Open();
+                    tableCmd.CommandText = $"INSERT INTO todo (name) VALUES ('{todo.Name}')";
+                    try
+                    {
+                        tableCmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+            return Redirect("https://localhost:7281/"); //zamiast 5001
+        }
+
+        [HttpPost]
+        public JsonResult Delete(int id)
+        {
+            using (SqliteConnection con =
+                   new SqliteConnection("Data Source=db.sqlite"))
+            {
+                using (var tableCmd = con.CreateCommand())
+                {
+                    con.Open();
+                    tableCmd.CommandText = $"DELETE from todo WHERE Id = '{id}'";
+                    tableCmd.ExecuteNonQuery();
+                }
+            }
+
+            return Json(new {});
+        }
+
+        public RedirectResult Update(TodoItem todo)
+        {
+            using (SqliteConnection con =
+                   new SqliteConnection("Data Source=db.sqlite"))
+            {
+                using (var tableCmd = con.CreateCommand())
+                {
+                    con.Open();
+                    tableCmd.CommandText = $"UPDATE todo SET name = '{todo.Name}' WHERE Id = '{todo.Id}'";
+                    try
+                    {
+                        tableCmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+
+            return Redirect("https://localhost:7281/"); //zamiast 5001
+        }
+    }
+}
+
+
+
+// public class HomeController : Controller
+// {
+//     private readonly ILogger<HomeController> _logger;
+
+//     public HomeController(ILogger<HomeController> logger)
+//     {
+//         _logger = logger;
+//     }
+
+//     public IActionResult Index()
+//     {
+//         var todoListViewModel = GetAllTodos();
+//         return View(todoListViewModel);
+//     }
+
+
+//     [HttpGet]
+//     public JsonResult PopulateForm(int id)
+//     {
+//         var todo = GetById(id);
+//         return Json(todo);
+//     }
+
+
+//     internal TodoViewModel GetAllTodos()
+//     {
+//         List<TodoItem> todoList = new();
+
+//         using (SqliteConnection con =
+//                 new SqliteConnection("Data Source=db.sqlite"))
+//         {
+//             using (var tableCmd = con.CreateCommand())
+//             {
+//                 con.Open();
+//                 tableCmd.CommandText = "SELECT * FROM todo";
+
+//                 using (var reader = tableCmd.ExecuteReader())
+//                 {
+//                     if (reader.HasRows)
+//                     {
+//                         while (reader.Read())
+//                         {
+//                             todoList.Add(
+//                                 new TodoItem
+//                                 {
+//                                     Id = reader.GetInt32(0),
+//                                     Name = reader.GetString(1)
+//                                 });
+//                         }
+//                     }
+//                     else
+//                     {
+//                         return new TodoViewModel
+//                         {
+//                             TodoList = todoList
+//                         };
+//                     }
+//                 };
+//             }
+//         }
+
+//         return new TodoViewModel
+//         {
+//             TodoList = todoList
+//         };
+//     }
+
+//         internal TodoItem GetById(int id)
+//         {
+//             TodoItem todo = new();
+
+//             using (var connection =
+//                    new SqliteConnection("Data Source=db.sqlite"))
+//             {
+//                 using (var tableCmd = connection.CreateCommand())
+//                 {
+//                     connection.Open();
+//                     tableCmd.CommandText = $"SELECT * FROM todo Where Id = '{id}'";
+
+//                     using (var reader = tableCmd.ExecuteReader())
+//                     {
+//                         if (reader.HasRows)
+//                         {
+//                             reader.Read();
+//                             todo.Id = reader.GetInt32(0);
+//                             todo.Name = reader.GetString(1);
+//                         }
+//                         else
+//                         {
+//                             return todo;
+//                         }
+//                     };
+//                 }
+//             }
+
+//             return todo;
+//         }
+
+
+
+//     internal TodoViewModel GetAllTodos()
+//     {
+//         List<TodoItem> todoList = new();
+
+//         using (SqliteConnection con =
+//                 new SqliteConnection("Data Source=db.sqlite"))
+//         {
+//             using (var tableCmd = con.CreateCommand())
+//             {
+//                 con.Open();
+//                 tableCmd.CommandText = "SELECT * FROM todo";
+
+//                 using (var reader = tableCmd.ExecuteReader())
+//                 {
+//                     if (reader.HasRows)
+//                     {
+//                         while (reader.Read())
+//                         {
+//                             todoList.Add(
+//                                 new TodoItem
+//                                 {
+//                                     Id = reader.GetInt32(0),
+//                                     Name = reader.GetString(1)
+//                                 });
+//                         }
+//                     }
+//                     else
+//                     {
+//                         return new TodoViewModel
+//                         {
+//                             TodoList = todoList
+//                         };
+//                     }
+//                 };
+//             }
+//         }
+
+//         return new TodoViewModel
+//         {
+//             TodoList = todoList
+//         };
+//     }
+
+//     internal TodoItem GetById(int id)
+//     {
+//         TodoItem todo = new();
+
+//         using (var connection =
+//                 new SqliteConnection("Data Source=db.sqlite"))
+//         {
+//             using (var tableCmd = connection.CreateCommand())
+//             {
+//                 connection.Open();
+//                 tableCmd.CommandText = $"SELECT * FROM todo Where Id = '{id}'";
+
+//                 using (var reader = tableCmd.ExecuteReader())
+//                 {
+//                     if (reader.HasRows)
+//                     {
+//                         reader.Read();
+//                         todo.Id = reader.GetInt32(0);
+//                         todo.Name = reader.GetString(1);
+//                     }
+//                     else
+//                     {
+//                         return todo;
+//                     }
+//                 };
+//             }
+//         }
+
+//         return todo;
+//     }
+
+//     public RedirectResult Insert(TodoItem todo)
+//     {
+//         using (SqliteConnection con =
+//                 new SqliteConnection("Data Source=db.sqlite"))
+//         {
+//             using (var tableCmd = con.CreateCommand())
+//             {
+//                 con.Open();
+//                 tableCmd.CommandText = $"INSERT INTO todo (name) VALUES ('{todo.Name}')";
+//                 try
+//                 {
+//                     tableCmd.ExecuteNonQuery();
+//                 }
+//                 catch (Exception ex)
+//                 {
+//                     Console.WriteLine(ex.Message);
+//                 }
+//             }
+//         }
+//         return Redirect("https://localhost:5010/");
+//     }
+
+//      [HttpPost]
+
+//     public JsonResult Delete(int id)
+//     {
+//         using (SqliteConnection con =
+//                 new SqliteConnection("Data Source=db.sqlite"))
+//         {
+//             using (var tableCmd = con.CreateCommand())
+//             {
+//                 con.Open();
+//                 tableCmd.CommandText = $"DELETE from todo WHERE Id = '{id}'";
+//                 tableCmd.ExecuteNonQuery();
+//             }
+//         }
+
+//         return Json(new {});
+//     }
+
+
+// }
