9fb1c481931e62c3ccedff6fac82912aed6f03d0
Lidka <75801399+lidiamoryc@users.noreply.github.com>
2024-06-05 17:42:26 +0400

    adding priority ocntroller

---
+++ Controllers/PriorityController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Models;
+using TaskManager.Repositories;
+
+namespace TaskManager.Controllers
+{
+    public class PriorityController : Controller
+    {
+        private readonly IPriorityRepository _priorityRepository;
+
+        public PriorityController(IPriorityRepository priorityRepository)
+        {
+            _priorityRepository = priorityRepository;
+        }
+
+        // Akcja do wyświetlenia listy priorytetów
+        public IActionResult Index()
+        {
+            var priorities = _priorityRepository.GetAllPriorities();
+            return View(priorities);
+        }
+
+        // Inne akcje do dodawania, edycji, usuwania itp., jeśli są potrzebne
+    }
+}
